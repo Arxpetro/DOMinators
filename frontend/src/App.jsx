789c734b882d3ec885456bc824f4ad03c7e2d40e
Anastasiia Myrka <myrkaa638@gmail.com>
2025-02-03 22:45:00 +0100

    footer/categories

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,16 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Header from "./Components/Header/Header";
-import HomePage from "./Components/HomePage/HomePage";
+import React from "react";
+import Footer from "./Components/Footer/Footer";
+import Map from "./Components/Map/Map";
+import Categories from "./Components/Categories/Categories";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<div className="main">
-				<div className="container">
-					<Header />
-					<Routes>
-						<Route path="/" element={<HomePage/>}/>
-					</Routes>
-					{/* Footer */}
-				</div>
-			</div>
-		</BrowserRouter>
-	);
+  return (
+    <div>
+      <Map />
+      <Footer />
+      <Categories />
+    </div>
+  );
 }
 
 export default App;
