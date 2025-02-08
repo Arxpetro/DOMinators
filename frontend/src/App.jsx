0063950ac36f781f6da668bbd3c06b87f5ed1f5d
Anastasiia Myrka <myrkaa638@gmail.com>
2025-02-08 11:52:25 +0100

    `Update react-router-dom to version 7.1.5 in frontend package-lock.json and package.json`

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./Components/Footer/Footer";
 import Map from "./Components/Map/Map";
 import Categories from "./Components/Categories/Categories";
 
 function App() {
   return (
-    <div>
-      <Map />
-      <Footer />
-      <Categories />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Map />} />
+          <Route path="/categories" element={<Categories />} />
+          
+        </Routes>
+        <Footer />
+        <Categories /> 
+         
+      </div>
+    </Router>
   );
 }
 
